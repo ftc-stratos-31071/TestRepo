927e219a54d7d3695c81d25e0d7daef88a34766e
Ritwik <ricksinha08@gmail.com>
2025-11-15 16:12:44 -0500

    added PID+limelight turret control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/PIDTest.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleops;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.ftc.components.BulkReadComponent;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import org.firstinspires.ftc.teamcode.subsystems.Turret;
+
+
+@Autonomous(name = "PIDTest")
+public class PIDTest extends NextFTCOpMode {
+    Limelight3A limelight;
+
+    double motorTargetX = 0.0;
+
+    @Override
+    public void onInit() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+        limelight.pipelineSwitch(0); // Switch to pipeline number 0
+    }
+
+    public PIDTest() {
+        addComponents(
+                new SubsystemComponent(Turret.INSTANCE),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
+    }
+
+    public Command firstRoutine(double degrees) {
+        return new ParallelGroup(
+                Turret.INSTANCE.runTurret(degrees)
+        );
+    }
+
+    @Override
+    public void onUpdate() {
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            double tx = result.getTx(); // How far left or right the target is (degrees)
+            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+
+            motorTargetX = tx;
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Area", ta);
+        } else {
+            telemetry.addData("Limelight", "No Targets");
+        }
+        firstRoutine(motorTargetX).invoke();
+        telemetry.update();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
@@ -1,19 +1,34 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
+
 import org.firstinspires.ftc.teamcode.constants.TurretConstants;
 
-import dev.nextftc.control.ControlSystem;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.control.ControlSystem;
 import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.controllable.RunToPosition;
 
+@Config
 public class Turret implements Subsystem {
     public static final Turret INSTANCE = new Turret();
-
-    private Turret() {}
+    private Turret() { }
 
     private final MotorEx turret = new MotorEx("turret");
-    private ControlSystem controlSystem = ControlSystem.builder()
+
+    private final ControlSystem turretController = ControlSystem.builder()
             .posPid(TurretConstants.kP, TurretConstants.kI, TurretConstants.kD)
             .build();
 
+    public Command runTurret(double degrees){
+        double clicksDegreeProportion = 1440.0/360.0;
+        double clicks = clicksDegreeProportion * degrees;
+        return new RunToPosition(turretController, clicks);
+    }
+
+    @Override
+    public void periodic() {
+        turret.setPower(turretController.calculate(turret.getState()));
+    }
 }
